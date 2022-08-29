794ec9c0c0216fcfd325a8acd159f96a042ff212
OscarNieto113 <a01705090@tec.mx>
2022-08-28 23:35:47 -0500

    Error en los bloques

--- cypher.h
+++ cypher.h
@@ -8,20 +8,30 @@ using namespace std;
 
 class Cypher{
     private:
-        unsigned char key;
+        vector<unsigned char> key[16];
         vector<unsigned char> block[16];
 
     public:
-        string encrypt (unsigned char, vector<unsigned char>);
-        string columnar_transposition (unsigned char);
-        string row_transposition (unsigned char);
-        string polyalphabetic_shift (unsigned char, vector<unsigned char>)
-        string xor_tecnique (unsigned char, vector<unsigned char>);
+        string encrypt (vector<unsigned char>, vector<unsigned char>);
+        string columnar_transposition (vector<unsigned char>);
+        string row_transposition (vector<unsigned char>);
+        string polyalphabetic_shift (vector<unsigned char>, vector<unsigned char>);
+        string xor_tecnique (vector<unsigned char>, vector<unsigned char>);
 
         string decrypt(unsigned char, vector<unsigned char>);
 };
 
 //string Cypher :: encrypt(unsigned char key, vector<unsigned char>block){
 //}
+
+string Cypher :: columnar_transposition(vector<unsigned char> block){
+    string example;
+    for (int i = 2; i < block.size(); i+=4){
+        example += block [i];
+    }
+
+    cout << example;
+    return example;
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,43 +1,93 @@
 #include <bits/stdc++.h>
 #include <fstream>
 #include <string>
+#include <math.h>
+#include <vector>
 
 using namespace std;
 
+const int SIZE_BLOCK = 16; //16 caracteres = 128 bits
+const char ADD_CHARACTER = 'x'; //Caracter con el que se va a rellenar si el bloque no esta completo
+
 #include "cypher.h"
+
+char menu(){
+    char option;
+    cout << endl << "Elige la opcion: " << endl;
+    cout << "1. Encriptar" << endl;
+    cout << "2. Desencriptar " << endl; 
+    cout << "3. Salir " << endl << endl; 
+    cin >> option;
+    cin.ignore();
+    return option;
+}
+
+string get_message_from_file(string file1){
+    string message;
+    ifstream file(file1);
+    if (file.is_open()){
+        while (getline(file, message)){
+                        
+        }
+        file.close();
+    }
+    else{
+        cout << "No se pudo abrir el archivo" << endl;
+    }
+    return message;
+}
+
+string fill_block(string message){
+    int message_difference = message.length() % SIZE_BLOCK;
+    if (message_difference != 0){
+        int aux = SIZE_BLOCK - (message_difference);
+        for (int i = aux; i > 0; i--){
+            message.push_back(ADD_CHARACTER);
+        }
+    }
+    return message;
+}
+
+vector <unsigned char> get_blocks(string message, int start, int end){
+    vector <unsigned char> block[SIZE_BLOCK];
+
+    int aux = 0;
+    for (int i = start; i < end; i++){
+        block[aux] = message.at(i);
+        aux++;
+    }
+    return block;
+}
+
 
 int main(int argc, char* argv[]) {
     char option;
     clock_t time_start, time_end;
     bool bandera = true;
+    int message_length;
+    
 
     while (bandera){
-        cout << "Elige la opcion: " << endl;
-        cout << "1. Encriptar" << endl;
-        cout << "2. Desencriptar " << endl; 
-        cout << "3. Salir " << endl << endl; 
-
-        cin >> option;
-        cin.ignore();
+        option = menu();
 
         switch (option){
             case '1': {
-                string password;
-                string message;
-                ifstream file("input.txt");
-                if (file.is_open()){
-                    while (getline(file, message)){
-                        
-                    }
-                    file.close();
-                }
-                else{
-                    cout << "No se pudo abrir el archivo" << endl;
-                }
-                cout << message;
-                cout << "Escribe una contraseña simetrica alfanumerica de no maximo 16 caracteres" << endl;
+                string password, message;
+                int number_of_blocks;
+                vector <unsigned char> block[SIZE_BLOCK];
+
+                string file = ("input.txt");
+                message = get_message_from_file(file);
+                cout << "Escribe una contraseña alfanumerica de tamanio de 16 caracteres" << endl;
                 cin >> password;
                 cin.ignore();
+                
+                message = fill_block(message);
+                number_of_blocks = message.size()/SIZE_BLOCK;
+
+                for (int i = 0; i < number_of_blocks; i++){
+                    block = get_blocks(message, i*SIZE_BLOCK, i*SIZE_BLOCK + SIZE_BLOCK);
+                }
 
                 //Realizar implementacion del encriptado con las variables message y password
                 break;
